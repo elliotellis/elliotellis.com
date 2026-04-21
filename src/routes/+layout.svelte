29484bf0f1d155a07b3390d65c3aef3fc6ca422a
Elliot Ellis <elliotellis@me.com>
2026-04-21 21:36:48 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
+
+<style>
+	:global {
+		body {
+			font-family: 'Delegate Trial';
+			font-size: 1rem;
+			line-height: 1.5;
+		}
+	}
+</style>
