4965cada5b5987393cd31264920adb765992eb7f
elliot ellis <elliotellis@me.com>
2026-04-22 12:09:42 +0100

    Initial base styles and head tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,17 @@
 </script>
 
 <svelte:head>
+  <meta property="og:locale" content="en_GB">
+	<link rel="icon" type="image/png" href={faviconPng1x} />
+	<link rel="icon" type="image/png" href={faviconPng2x} />
 	<link rel="icon" href={favicon} />
+  <link rel="canonical" href={'https://elliotellis.com' + page.url.pathname} />
+  <meta property="og:url" content={'https://elliotellis.com' + page.url.pathname}>
+  <meta name="twitter:url" content={'https://elliotellis.com' + page.url.pathname}>
+  <meta property="og:site_name" content="elliot ellis">
+  <meta property="og:see_also" content="https://elliotellis.com">
+  <meta name="twitter:card" content="summary">
+  <script defer src="https://cloud.umami.is/script.js" data-website-id="1e454313-0ae1-4523-a698-230e19d476c8"></script>
 </svelte:head>
 
 {@render children()}
@@ -13,10 +23,56 @@
 
 <style>
 	:global {
-		body {
-			font-family: 'Delegate Trial';
-			font-size: 1rem;
-			line-height: 1.5;
+		*, *::before, *::after {
+      box-sizing: border-box;
+    }
+
+		:root {
+			--colour-white: #fff;
+			--colour-lightgrey: #eee;
+			--colour-midgrey: #9e9896;
+			--colour-black: #23202e; /* to change to new black */
+			--colour-orange: #ee6416;
+
+			--typeface: 'Delegate Trial', 'Tahoma', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
+			--fontweight-reg: 400;
+			--fontweight-bold: 700;
+			--leading-base: 1.25em;
+
+			--typesize-body: 1rem;
+			--leading-body: 1.25rem;
 		}
+
+		body {
+			--background-colour: var(--colour-white);
+      --text-colour: var(--colour-black);
+      --key-colour: var(--colour-orange);
+
+			font-family: var(--typeface);
+      line-height: var(--leading-base);
+      text-rendering: optimizeLegibility;
+      -moz-osx-font-smoothing: grayscale;
+      -webkit-font-smoothing: antialiased;
+      background: var(--background-colour);
+      color: var(--text-colour);
+      min-width: 320px;
+      min-height: 100vh;
+			margin: 0;
+      display: grid;
+      grid-template-rows: 1fr auto;
+      position: relative;
+		}
+
+		em { font-style: italic; }
+    strong { font-weight: var(--fontweight-bold); }
+
+    a { color: var(--text-colour); }
+    a:hover { color: var(--text-colour); }
+
+		/* a[target="_blank"]:after {
+      content: '\2197';
+    } */
+
+		
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+  <!-- Page title -->
+  <title>elliot ellis &ndash; {data.siteSettings[0].siteSubheading}</title>
+  <meta property="og:title" content={data.siteSettings[0].siteSubheading + ' &ndash; elliot ellis'}>
+  <meta property="twitter:title" content={data.siteSettings[0].siteSubheading + ' &ndash; elliot ellis'}>
+  <!-- Page image -->
+  {#if data.siteSettings[0].siteSharingImage}
+    <meta property="og:image" content={data.siteSettings[0].siteSharingImage}>
+    <meta name="twitter:image" content={data.siteSettings[0].siteSharingImage}>
+  {/if}
+  <!-- Page description -->
+  {#if data.siteSettings[0].siteDescription}
+    <meta property="og:description" content={data.siteSettings[0].siteDescription}>
+    <meta name="twitter:description" content={data.siteSettings[0].siteDescription}>
+  {/if}
+</svelte:head>
+
+<main></main>
+
+<style></style>
