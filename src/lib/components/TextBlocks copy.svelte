533a067006994ce68d3fd5370ead3312a994e6ae
elliot ellis <elliotellis@me.com>
2026-06-30 02:15:55 +0100

    text reflow: back to being buggy but at least much more succinct now

--- src/lib/components/TextBlocks copy.svelte
+++ src/lib/components/TextBlocks copy.svelte
@@ -1,27 +1,22 @@
 <script>
-  import { tick } from 'svelte';
-  let { blocks = [] } = $props();
-  let containerElement = $state(null);
+  import Paragraph from '$lib/components/Paragraph.svelte'
+  let { children, blocks = [] } = $props();
   let blockIndents = $state([0]);
   function getNextIndent(i) {
     return (element) => {
-      tick().then(() => {
-        const range = document.createRange();
-        range.selectNodeContents(element);
-        const rects = range.getClientRects();
-        blockIndents[i+1] = rects[rects.length - 1].width;
-        $inspect(blockIndents);
-      });
+      const range = document.createRange();
+      range.selectNodeContents(element);
+      const rects = range.getClientRects();
+      blockIndents.push(rects[rects.length - 1].width);
+      //blockIndents[i+1] = rects[rects.length - 1].width;
+      $inspect(element);
     }
   }
 </script>
-<!-- {@attach blockIndents.length <= blocks.length && getNextIndent(i)}  -->
-<div class="text-container" bind:this={containerElement}>
+
+<div class="text-container">
   {#each blocks as block, i (i)}
-    <p 
-      {@attach blockIndents.length <= blocks.length && getNextIndent(i)} 
-      style:text-indent={blockIndents[i] + 'px'}
-    >
+    <p {@attach blockIndents.length <= blocks.length && getNextIndent(i)} style:text-indent={blockIndents[i] + 'px'}>
       {#each block.portions as portion}
         {#if portion.className}
           <span class={portion.className}>{portion.text}</span>
--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,9 @@ const config = {
 	compilerOptions: {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true),
-		/*experimental: {
+		experimental: {
 			async: true
-		},*/
+		},
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
