e113bea92e53ab91210aa6b91b734d3b3e5d7e25
elliot ellis <elliotellis@me.com>
2026-07-03 13:05:34 +0100

    Kirby test fetch

---
+++ src/routes/+page.js
@@ -0,0 +1,14 @@
+async function getData() {
+  const url = 'https://ee-lib.test/home.json';
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
