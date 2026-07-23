1f237995b028b4e56a15ac93a34bfe47d706032f
elliot ellis <elliotellis@me.com>
2026-07-23 15:38:02 +0100

    Fix +page.js export variable name

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,6 @@
 import { PUBLIC_KIRBY_SERVER_URL } from '$env/static/public';
 export async function load({ fetch, params }) {
 	const res = await fetch(`${PUBLIC_KIRBY_SERVER_URL}/works.json`);
-	const works = await res.json();
-	return { works };
+	const data = await res.json();
+	return { data };
 }
