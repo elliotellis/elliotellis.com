ca37aa192d74404bafc6d73c6afd8810f3cc411d
elliot ellis <elliotellis@me.com>
2026-07-23 15:35:40 +0100

    Environment vars for kirby api endpoint

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,6 @@
+import { PUBLIC_KIRBY_SERVER_URL } from '$env/static/public';
 export async function load({ fetch, params }) {
-	const res = await fetch(`https://eell.io/works.json`);
-	const data = await res.json();
-	return { data };
+	const res = await fetch(`${PUBLIC_KIRBY_SERVER_URL}/works.json`);
+	const works = await res.json();
+	return { works };
 }
